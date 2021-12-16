e8e8fb24b001188844aeb9dc26ec6f55af5e2264
MatWsQ <miatiby@gmail.com>
2021-12-16 20:23:50 +0100

    Ćwiczenie 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,60 +4,68 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* ĆW#2 */
-        byte zmiennaByte = 22;
-        short zmiennaShort = 1231;
-        int zmiennaInt = 10213;
-        long zmiennaLong = 123000123000L;
-        float zmiennaFloat = 22.12f;
-        double zmiennaDouble = 129000.1299;
-        boolean zmiennaBoolean = true;
-        char zmiennaChar = 't';
-        String zmiennaString = "Witaj świecie!";
-        int liczbaA = 25;
-        int liczbaB = 5;
-        double liczbaX = 20.5;
-        double liczbaY = 5.5;
-        System.out.println("Wyświetlanie zmiennych: ");
 
-        System.out.println(zmiennaByte);
-        System.out.println(zmiennaShort);
-        System.out.println(zmiennaInt);
-        System.out.println(zmiennaLong);
-        System.out.println(zmiennaFloat);
-        System.out.println(zmiennaDouble);
-        System.out.println(zmiennaBoolean);
-        System.out.println(zmiennaChar);
-        System.out.println(zmiennaString);
-        System.out.println(liczbaA);
-        System.out.println(liczbaB);
-        System.out.println(liczbaX);
-        System.out.println(liczbaY);
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+        boolean d = false;
+        System.out.println("Operatory arytmetyczne:" + "\na = true\tb = false\tc = true\td= false");
 
-        System.out.println(" ");
+        System.out.println("(a && b) : " + (a && b));
+        System.out.println("(a && c) : " + (a && c));
+        System.out.println("(a && d) : " + (a && d));
+        System.out.println("(b && a) : " + (b && a));
+        System.out.println("(b && c) : " + (b && c));
+        System.out.println("(b && d) : " + (b && d));
+        System.out.println("(c && a) : " + (c && a));
+        System.out.println("(c && b) : " + (c && b));
+        System.out.println("(c && d) : " + (c && d));
+        System.out.println("(d && a) : " + (d && a));
+        System.out.println("(d && b) : " + (d && b));
+        System.out.println("(d && c) : " + (d && c));
 
-        System.out.println("Dodawanie zmiennych typu INT"+" "+ (liczbaA + liczbaB));
-        System.out.println("Odejmowanie zmiennych typu INT"+" "+ (liczbaA - liczbaB));
-        System.out.println("Mnożenie zmiennych typu INT"+" "+ (liczbaA * liczbaB));
-        System.out.println("Dzielenie zmiennych typu INT"+" "+ (liczbaA / liczbaB));
-        System.out.println("Modulo zmiennych typu INT"+" "+ (liczbaB % liczbaA));
-        System.out.println("Inkrementacja zmiennej LiczbaA"+" "+ (++liczbaA));
-        System.out.println("Inkrementacja zmiennej LiczbaB"+" "+ (++liczbaB));
-        System.out.println("Dekrementacja zmiennej LiczbaA"+" "+ (--liczbaA));
-        System.out.println("Dekrementacja zmiennej LiczbaB"+" "+ (--liczbaB));
+        System.out.println("");
 
-        System.out.println(" ");
+        System.out.println("(a || b) : " + (a || b));
+        System.out.println("(a || c) : " + (a || c));
+        System.out.println("(a || d) : " + (a || d));
+        System.out.println("(b || a) : " + (b || a));
+        System.out.println("(b || c) : " + (b || c));
+        System.out.println("(b || d) : " + (b || d));
+        System.out.println("(c || a) : " + (c || a));
+        System.out.println("(c || b) : " + (c || b));
+        System.out.println("(c || d) : " + (c || d));
+        System.out.println("(d || a) : " + (d || a));
+        System.out.println("(d || b) : " + (d || b));
+        System.out.println("(d || c) : " + (d || c));
 
-        System.out.println("Dodawanie zmiennych typu Double"+" "+ (liczbaX + liczbaY));
-        System.out.println("Odejmowanie zmiennych typu Double"+" "+ (liczbaX - liczbaY));
-        System.out.println("Mnożenie zmiennych typu Double"+" "+ (liczbaX + liczbaY));
-        System.out.println("Dzielenie zmiennych typu Double"+" "+ (liczbaX - liczbaY));
-        System.out.println("Modulo zmiennych typu Double"+" "+ (liczbaY % liczbaX));
-        System.out.println("Inkrementacja zmiennej LiczbaX"+" "+ (++liczbaX));
-        System.out.println("Inkrementacja zmiennej LiczbaY"+" "+ (++liczbaY));
-        System.out.println("Dekrementacja zmiennej LiczbaX"+" "+ (--liczbaX));
-        System.out.println("Dekrementacja zmiennej LiczbaY"+" "+ (--liczbaY));
+        System.out.println("");
 
+        System.out.println("!a : " + !a);
+        System.out.println("!b : " + !b);
+        System.out.println("!c : " + !c);
+        System.out.println("!d : " + !d);
+
+        System.out.println("");
+
+        System.out.println("!(a && b) : " + !(a && b));
+        System.out.println("!(b && c) : " + !(b && c));
+        System.out.println("!(c && d) : " + !(c && d));
+
+        System.out.println("");
+
+        System.out.println("!((a && c) || (!a || b) && (!a || d) : " + !((a && c) || (!a || b) && (!a || d)));
+        System.out.println("!((a && c) && (!a || b) && (!a || d) || (!d || b) : " + !((a && c) && (!a || b) && (!a || d) || (!d || b)));
+
+        System.out.println();
+
+        int x = 2;
+        int y = 5;
+        int z = 2;
+
+        System.out.printf("Operatory porównania:" + "\nx = %d\ty = %d\tz = %d\n", x, y, z);
+
+        System.out.println("((x > y) || (a && b) || (b && d) && (x <= z)) : " + ((x > y) || (a && b) || (b && d) && (x <= z)));
     }
 }
 
